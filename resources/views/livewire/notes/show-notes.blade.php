b71f7f620feb24898d14f23faba99c1cdfa0f93b
Dominik Hofer <hi@dominikhofer.me>
2024-07-25 17:27:19 +0200

    Added recipient to notes

--- resources/views/livewire/notes/show-notes.blade.php
+++ resources/views/livewire/notes/show-notes.blade.php
@@ -1,6 +1,7 @@
 <?php
 
 use Livewire\Volt\Component;
+use Carbon\Carbon;
 
 new class extends Component {
     public function with()
@@ -12,9 +13,22 @@ new class extends Component {
 }; ?>
 
 <div>
-    <ul>
-        @foreach ($testNotes as $note)
-            <li>{{ $note->title }}</li>
+    <div class="grid grid-cols-2 gap-4 mt-12">
+        @foreach ($notes as $note)
+            <x-card wire:key='{{ $note->id }}'>
+                <div class="flex justify-between">
+                    <a href="#"
+                        class="text-xl font-bold hover:text-blue-500 hover:underline">{{ $note->title }}</a>
+                    <div class="text-gray-500 text-sx">{{ Carbon::parse($note->send_date)->format('d/m/Y') }}</div>
+                </div>
+                <div class="flex items-end justify-between gap-1 mt-4">
+                    <p class="text-sx">Recipient: <span class="font-semibold">{{ $note->recipient }}</span></p>
+                    <div>
+                        <x-mini-button rounded outline secondary icon="eye"></x-mini-button>
+                        <x-mini-button rounded outline secondary icon="trash"></x-mini-button>
+                    </div>
+                </div>
+            </x-card>
         @endforeach
-    </ul>
+    </div>
 </div>
